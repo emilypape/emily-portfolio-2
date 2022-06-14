04d0a8d81117bd9d3ee902b5f2e5dc74c83b6b09
emily <eap6787@gmail.com>
2022-06-13 20:45:17 -0600

    finish contact portion

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
 
 function Contact({ toggleModal, isModalOpen }) {
   const handleClick = (e) => {
@@ -13,7 +14,22 @@ function Contact({ toggleModal, isModalOpen }) {
           <div>
             <h1 className='modal-contact-header'>Contact Me</h1>
             <span></span>
-            <p>Some text in the Modal..</p>
+            <div className='contact-icon-box'>
+              <a target='_blank' rel='noreferrer' href='https://github.com/emilypape' className='contact-icon'>
+                <Icon className='icon-styles' icon='akar-icons:github-fill' />
+              </a>
+              <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/feed/' className='contact-icon'>
+                <Icon className='icon-styles' icon='logos:linkedin-icon' />
+              </a>
+              <a href='mailto:eap6787t@gmail.com' className='contact-icon'>
+                <Icon className='icon-styles' icon='logos:google-gmail' />
+              </a>
+            </div>
+            <div className='fashion-line-2'>.</div>
+            <h1 className='contact-message'>
+              Thank you for your interest, I can't wait to hear from you! Feel free to use the above links to get in
+              touch.
+            </h1>
           </div>
         </div>
       </div>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,11 +5,17 @@ function Footer() {
   return (
     <footer className='footer-styles'>
       <div className='footer-box'>
-        <Icon icon='akar-icons:github-fill' />
+        <a target='_blank' rel='noreferrer' href='https://github.com/emilypape'>
+          <Icon icon='akar-icons:github-fill' />
+        </a>
         &nbsp;&nbsp;&nbsp;
-        <Icon icon='logos:linkedin-icon' />
+        <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/feed/'>
+          <Icon icon='logos:linkedin-icon' />
+        </a>
         &nbsp;&nbsp;&nbsp;
-        <Icon icon='logos:google-gmail' />
+        <a href='mailto:eap6787t@gmail.com'>
+          <Icon icon='logos:google-gmail' />
+        </a>
       </div>
     </footer>
   );
